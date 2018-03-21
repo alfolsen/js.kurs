06bdf448ca4f81346e739c0bf8a8c9320a4d58f8
alfolsen <37101854+alfolsen@users.noreply.github.com>
2018-03-21 14:02:28 +0100

    Made some js tryes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-function hello(){
-    console.log("works");   
+var customHeader = "something";
+var heading = document .querySelector(`h1`);
+heading .textContent = customHeader;
+
+var number1 = 2;
+var number2 = 3;
+var text = "fancy shitt";
+
+var myList = ["one", "two", "three"]
+var myObject = {};
+
+function multyplyByTwo(n1, n2){
+    var val = n2 + n1;
+return val * 2;
+}
+
+function addText(){
+    document.getElementById("textChange").innerHTML = el;
+    if (el == ""){
+        el.innerText = "rofls"
+    }
+    else{
+        el.innerText = ""
+    }
 }
